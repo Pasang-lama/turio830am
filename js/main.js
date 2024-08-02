34ab0d6d48aede82d5b5c8b1e3090bcd4f8a6d83
Pasang-lama <lpasang182@gmail.com>
2024-08-02 08:40:07 +0545

    works on the responsive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $(".banner").owlCarousel({
-        items: 1,
-        autoplay : true,
-        loop : true,
-        nav: true,
-        dots: false,
-        navText: ['<i class="fa-solid fa-arrow-up"></i>','<i class="fa-solid fa-arrow-down"></i>']
-    });
+    // $(".banner").owlCarousel({
+    //     items: 1,
+    //     autoplay : true,
+    //     loop : true,
+    //     nav: true,
+    //     dots: false,
+    //     // margin : 20,
+    //     navText: ['<i class="fa-solid fa-arrow-up"></i>','<i class="fa-solid fa-arrow-down"></i>']
+    // });
 
     $(".best-selling-tours-slider").owlCarousel({
         items: 3,
@@ -14,6 +15,17 @@ $(document).ready(function(){
         nav: true,
         dots: false,
         navText: ['<i class="fa-solid fa-arrow-left-long"></i>','<i class="fa-solid fa-arrow-right-long"></i>'],
-        margin: 50
+        margin: 40,
+        responsive : {
+            0 : {
+                items: 1,
+            },
+            768 : {
+                items: 2,
+            },
+            991 : {
+                items: 3,
+            }
+        }
     });
   });
